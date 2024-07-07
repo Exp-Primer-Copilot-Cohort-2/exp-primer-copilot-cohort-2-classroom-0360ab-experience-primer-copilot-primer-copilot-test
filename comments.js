625f80b3660e64de7bc31a8274f39909126d9170
Saugat Tamang <itsurenkanakueta@gmail.com>
2024-07-07 15:40:46 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,4 @@
+// Create web server
+// Create a web server that listens on port 3000. The server will read the file specified in the URL and return its content. If the file does not exist, it will return a 404 response. 
+// 
+// The file path is extracted from the URL, and the file is read using the fs.readFile method. The content is then returned as the response. If the file does not exist, a 404 response is sent.
