2768762e3efa3e0d0aa3fb16ce31a0d9595d0381
Juan Pablo Acosta <113045937+JPablo67@users.noreply.github.com>
2024-09-22 19:02:48 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,27 @@
-// create web server
+//create web server
 const express = require('express');
 const app = express();
-const port = 3000;
-// create a route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const url = require('url');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(express.static('views'));
+app.set('view engine', 'ejs');
+
+let comments = [];
+
+app.get('/comments', (req, res) => {
+    res.render('comments', {comments: comments});
 });
-// listen on port
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+
+app.post('/comments', (req, res) => {
+    let comment = req.body.comment;
+    comments.push(comment);
+    res.render('comments', {comments: comments});
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
