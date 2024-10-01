d5b378114234d185c147b4c9aa805a131332f557
Bruno S. Duarte <brunosduarte@hotmail.com>
2024-10-01 18:12:07 +0000

    Copilot second commit

--- skills.js
+++ skills.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-function calculateNumbers(var1, var2) { 
-  console.log(var1 + var2)
-}
-=======
 function calculateNumbers(var1, var2) {
   console.log(var1 + var2)
 }
->>>>>>> a91da1a8614ece133b2d62c11522ef9617ca8933
